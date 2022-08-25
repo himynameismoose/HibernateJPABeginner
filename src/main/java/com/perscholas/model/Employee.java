224360890dbd0b8619b6685e29e761014eb33c8f
Mershelle Rivera <mershellemae@gmail.com>
2022-08-25 14:11:06 -0700

    add @namedquery/@namedqueries annotation for single/multiple hql queries

--- src/main/java/com/perscholas/model/Employee.java
+++ src/main/java/com/perscholas/model/Employee.java
@@ -6,6 +6,29 @@ import java.util.Date;
 
 @Entity
 @Table
+// Using @NamedQuery for single HQL
+@NamedQuery(name = "getAllEmployees", query = "SELECT e FROM Employee e")
+
+// Using @NamedQueries for multiple HQL
+@NamedQueries({
+        @NamedQuery(name = "get_emp_name_by_id",
+                query = "SELECT e.name, e.salary, e.job" +
+                        "FROM Employee e" +
+                        "WHERE id=:id"),
+        @NamedQuery(name = "get_all_dept", query = "FROM Employee"),
+        @NamedQuery(name = "findEmployeeByName",
+                query = "FROM Employee e WHERE e.name=:name"),
+        @NamedQuery(name = "empDeptAlias",
+                query = "SELECT e, e.officeCode, e.name," +
+                        "CASE WHEN (e.officeCode = '1') THEN 'IT'" +
+                        "WHEN (e.officeCode = '6') THEN 'Admin'" +
+                        "WHEN (e.officeCode = '5') THEN 'HR'" +
+                        "WHEN (e.officeCode = '4') THEN 'Developers'" +
+                        "WHEN (e.officeCode = '3') THEN 'Accounts'" +
+                        "WHEN (e.officeCode = '2') THEN 'Finance'" +
+                        "ELSE 'General'" +
+                        "END FROM Employee e"),
+})
 public class Employee {
 
     @Id
